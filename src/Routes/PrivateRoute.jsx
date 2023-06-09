b303e0cf094fd897e13c074081d5377c4088406f
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-09 20:48:15 +0600

    privateroute page added

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useAuth()
+    const location = useLocation();
+    if(loading) {
+        return <progress className="progress w-56"></progress>;
+    }
+    if(user){
+        return children;
+    }
+    return (
+      <Navigate state={{from: location}} replace to="/login"></Navigate>
+    );
+};
+
+export default PrivateRoute;
