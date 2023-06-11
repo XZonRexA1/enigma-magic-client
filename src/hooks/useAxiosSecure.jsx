c1290feade7be93ba06a5fd0d4b0ec92cd1adc2a
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-12 03:00:24 +0600

    vercel link / removed

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
-  baseURL: 'https://enigma-magic-server.vercel.app/', 
+  baseURL: 'https://enigma-magic-server.vercel.app', 
 });
 
 const useAxiosSecure = () => {
