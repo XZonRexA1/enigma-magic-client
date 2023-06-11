e2f456220b573ca480204e05ad6d56dd9f67e1a5
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-11 21:06:26 +0600

    social login user saved to the database

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -13,7 +13,21 @@ const SocialLogin = () => {
     googleSingIn().then((result) => {
       const loggedInUser = result.user;
       console.log(loggedInUser);
-      navigate(from, { replace: true });
+      const saveUser = {
+        name: loggedInUser.displayName,
+        email: loggedInUser.email,
+      };
+      fetch("http://localhost:5000/users", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(saveUser),
+      })
+        .then((res) => res.json())
+        .then(() => {
+          navigate(from, { replace: true });
+        });
     });
   };
 
